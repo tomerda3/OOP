51aee267f94bdc04d39d3f4301761d0906c52396
navesf <navesf@ac.sce.ac.il>
2022-03-23 21:06:14 +0200

    Update UnusualFish.java

--- HomeWork_1/src/q3/UnusualFish.java
+++ HomeWork_1/src/q3/UnusualFish.java
@@ -1,16 +1,16 @@
 package q3;
 
-public class UnusualFish extends Fish{
+public class UnusualFish extends Fish {
 
     private int factor ;
 
-    public UnusualFish(int factor, int distance_eat, int size, int col, int x_front, int y_front){
-        super(distance_eat, size, col, x_front, y_front);
+    public UnusualFish(int horSpeed, int verSpeed, int distance_eat, int size, int col, int x_front, int y_front, int factor) {
+        super(horSpeed, verSpeed, distance_eat, size, col, x_front, y_front);
         this.factor = factor;
     }
 
-    public int getSize(){
-        return factor*super.getSize();
+    public int getSize() {
+        return factor * super.getSize();
     }
 
     public int getFactor() {
@@ -21,4 +21,20 @@ public class UnusualFish extends Fish{
         this.factor = factor;
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + "UnusualFish{" +
+                "factor=" + factor +
+                '}';
+    }
+
+    public boolean equals(UnusualFish unusualFish) {
+        if (this == unusualFish)
+            return true;
+        if (unusualFish == null || getClass() != unusualFish.getClass())
+            return false;
+        if (!super.equals(unusualFish))
+            return false;
+        return factor == unusualFish.factor;
+    }
 }
