fb1b5e13fded812978ef55b5ad4839dbbf99b26d
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-03-23 18:27:53 +0200

    Update Location.java

--- HomeWork_1/src/q2/Location.java
+++ HomeWork_1/src/q2/Location.java
@@ -1,11 +1,13 @@
 package q2;
 
+import java.time.LocalDate;
+
 public class Location {
 
     String name;
     Temperature[] temp;
 
-    public Location(String name){
+    public Location(String name) {
         this.name = name;
     }
 
@@ -17,30 +19,63 @@ public class Location {
         this.name = name;
     }
 
-    public void printLocation(){
-        System.out.println(name);
-        if (temp.length > 0)
-            for (int i=0 ; i < temp.length; i++)
-                System.out.println(temp[i]);
-        else System.out.println("no temperature measurements available");
+    public void printLocation() {
+        System.out.print(name + "temperature measurements: ");
+        if (temp != null)
+            for (Temperature temperature : temp)
+                System.out.print(temperature + "|");
+        else
+            System.out.print("no temperature measurements available.");
+        System.out.println();
     }
-    public double getAverage(){
 
+    public void printLocation(double n) {
+        System.out.print(name + "temperature measurements: ");
+        if (temp != null)
+            for (Temperature temperature : temp) {
+                if (temperature.getScale() >= Math.abs(n - getAverage()) && temperature.getScale() <= n + getAverage())
+                    System.out.print(temperature + "|");
+            }
+        else
+            System.out.print("no temperature measurements available.");
+        System.out.println();
+    }
+
+    public double getAverage() {
         double count =0;
-
-        if (temp.length ==0)
+        if (temp == null)
             return 0;
-
-        for (int i=0; i < temp.length; i++){
-            count += temp[i].getScale();
-        }
+        for (Temperature temperature : temp)
+            count += temperature.getScale();
         return count/temp.length;
     }
 
-//    public void addTemp(double scale,  ) {
-//
-//    }
-//    public void addTemp(double scale,  ) {
-//
-//    }
+    public void addTemp(double scale, int d, int m, int y) {
+        Temperature temp = new Temperature(scale, d, m, y);
+        Temperature [] arr;
+        if (this.temp == null)
+            arr = new Temperature[1];
+        else
+            arr = new Temperature[this.temp.length+1];
+        int i;
+        for (i = 0; i < arr.length-1; i++)
+            arr[i] = this.temp[i];
+        arr[i] = temp;
+        this.temp = arr;
+    }
+
+    public void addTemp(double scale) {
+        LocalDate date = LocalDate.now();
+        addTemp(scale, date.getDayOfMonth(), date.getMonthValue(), date.getYear());
+    }
+
+    public Temperature getMax() {
+        if (temp == null)
+            return null;
+        Temperature max = new Temperature(temp[0]);
+        for (int i = 1; i < temp.length; i++)
+            if (max.getScale() < temp[i].getScale())
+                max = new Temperature(temp[i]);
+        return max;
+    }
 }
