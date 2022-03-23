a1a9022da920845daba212d9ef9e365b8ee3f41f
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-03-23 18:28:11 +0200

    Create Swimmable.java

---
+++ HomeWork_1/src/q3/Swimmable.java
@@ -0,0 +1,39 @@
+package q3;
+
+public abstract class Swimmable implements Comparable {
+
+    protected int horSpeed;
+    protected int verSpeed;
+
+    public Swimmable() {}
+
+    public Swimmable(int horSpeed, int verSpeed) {
+        this.horSpeed = horSpeed;
+        this.verSpeed = verSpeed;
+    }
+
+    public int getHorSpeed() {
+        return horSpeed;
+    }
+
+    public int getVerSpeed() {
+        return verSpeed;
+    }
+
+    public boolean setHorSpeed(int horSpeed) {
+        this.horSpeed = horSpeed;
+        return true;
+    }
+
+    public boolean setVerSpeed(int verSpeed) {
+        this.verSpeed = verSpeed;
+        return true;
+    }
+
+    public abstract String getAnimalName();
+    public abstract int getEatCount();
+    public abstract int getSize();
+    public abstract String getColor();
+    public abstract void eatInc();
+
+}
