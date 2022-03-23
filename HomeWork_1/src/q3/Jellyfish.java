89ab3b69ae26509ea2d65d84b8e9536897748bc0
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-03-23 19:13:38 +0200

    Create Jellyfish.java

---
+++ HomeWork_1/src/q3/Jellyfish.java
@@ -0,0 +1,55 @@
+package q3;
+
+public class Jellyfish extends Swimmable{
+    private int DISTANCE_EAT;
+    private int size;
+    private int col;
+    private int eatCount;
+    private int x_front, y_front, x_dir, y_dir;
+
+    public Jellyfish(int distance_eat, int size, int col, int x_front, int y_front) {
+        DISTANCE_EAT = distance_eat;
+        this.size = size;
+        this.col = col;
+        this.eatCount = 0;
+        this.x_front = x_front;
+        this.y_front = y_front;
+        this.x_dir = 1;
+        this.y_dir = 1;
+    }
+    public String getColor() {
+        String[] colors = {"Black", "Red", "Blue", "Green", "Cyan", "Orange", "Yellow", "Magenta", "Pink"};
+        return colors[col-1];
+    }
+    public void changeJellyfish(int size) {
+        this.size = size;
+    }
+
+
+    public String getAnimalName() {
+        return "JellyFish";
+    }
+
+    public int getDISTANCE_EAT() {
+        return DISTANCE_EAT;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public int getEatCount() {
+        return eatCount;
+    }
+
+    public void eatInc() {
+        eatCount++;
+        if (eatCount > DISTANCE_EAT) {
+            eatCount = 0;
+            size++;
+        }
+    }
+
+}
