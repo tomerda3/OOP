85395ce1bca1a94f75a4646a93274ebcb8d608d0
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-03-23 18:28:16 +0200

    Update Utility.java

--- HomeWork_1/src/start/Utility.java
+++ HomeWork_1/src/start/Utility.java
@@ -1,34 +1,39 @@
 package start;
 
 import q1.MyDate;
+import q2.Location;
 
 public class Utility {
 
     public static MyDate threeDates(MyDate [] dates) {
-
-        MyDate temp1 ;
-        MyDate temp2 ;
-        int count =0;
-        for (int i =0; i< dates.length ; i++){
-
-            count =0;
-            temp1= dates[i].nextDate();
-            temp2= dates[i].nextDate().nextDate();
-            for (int j=0;j<dates.length; j++){
-
-                if (dates[j].equals(temp1) )
-                    count ++;
-            }
-            for (int k=0; k<dates.length; k++){
-                if (dates[k].equals(temp2))
-                    count ++;
-
-            }
-
-            if (count ==2){
-                return dates[i];
-            }
+        MyDate temp1, temp2;
+        int count;
+        for (MyDate date : dates) {
+            count = 0;
+            temp1 = date.nextDate();
+            temp2 = temp1.nextDate();
+            for (MyDate myDate : dates)
+                if (myDate.equals(temp1))
+                    count++;
+            for (MyDate myDate : dates)
+                if (myDate.equals(temp2))
+                    count++;
+            if (count == 2)
+                return date;
         }
         return new MyDate();
+    }
+
+    public static int getMaxTemp(Location [] arr) {
+        if (arr.length == 0)
+            return -1;
+        double max = arr[0].getAverage();
+        int index = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (max < arr[i].getAverage()) {
+                max = arr[i].getAverage();
+                index = i;
+            }
+        return index;
     }
 }
