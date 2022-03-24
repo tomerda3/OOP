236133e9fbfac31f8afae54a15d5eaea2f4ffcb0
navesf <navesf@ac.sce.ac.il>
2022-03-24 16:42:01 +0200

    Update Fish.java

--- HomeWork_1/src/q3/MultiColorFish.java
+++ HomeWork_1/src/q3/MultiColorFish.java
@@ -2,7 +2,7 @@ package q3;
 
 public class MultiColorFish extends Fish {
 
-    public MultiColorFish(int size, int horSpeed, int verSpeed, int x_front, int y_front, int col){
+    public MultiColorFish(int size, int x_front, int y_front, int horSpeed, int verSpeed, int col){
         super(size,horSpeed, verSpeed, x_front, y_front, col);
     }
 
